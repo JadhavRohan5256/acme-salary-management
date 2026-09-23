51842ad80e190cf23738b1f3058c98976d613c3b
Rohan Jadhav <jadhavrohan5050@gmail.com>
2026-09-23 20:36:29 +0530

    feat(frontend): added ngrx authentication and login

--- frontend/src/app/features/auth/login/login.component.spec.ts
+++ frontend/src/app/features/auth/login/login.component.spec.ts
@@ -1,23 +1,63 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    })
-    .compileComponents();
-    
+      declarations: [
+        LoginComponent
+      ],
+      imports: [
+        ReactiveFormsModule
+      ],
+      providers: [
+        provideMockStore()
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+
+    store = TestBed.inject(MockStore);
+    spyOn(store, 'dispatch');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have invalid form initially', () => {
+    expect(component.loginForm.invalid).toBeTrue();
+  });
+
+  it('should make form valid when username and password are provided', () => {
+    component.loginForm.setValue({
+      username: 'admin',
+      password: 'password'
+    });
+
+    expect(component.loginForm.valid).toBeTrue();
+  });
+
+  it('should not dispatch login when form is invalid', () => {
+    component.submit();
+    expect(store.dispatch).not.toHaveBeenCalled();
+  });
+
+  it('should dispatch login when form is valid', () => {
+    component.loginForm.setValue({
+      username: 'admin',
+      password: 'password'
+    });
+    component.submit();
+    expect(store.dispatch).toHaveBeenCalled();
+  });
+
 });
